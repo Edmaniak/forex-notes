6c6cbfa5a48fea4c6fd29ed244f33e70a029bd4b
Edmaniak <adam.ouhrabka@gmail.com>
2019-07-23 20:58:52 +0200

    instruments api complete

--- backend/models/instrument.js
+++ backend/models/instrument.js
@@ -1,17 +1,47 @@
 const mongoose = require('mongoose');
 
 const instrumentSchema = mongoose.Schema({
-	name: String,
-	onFire: Boolean,
-	timeFrames: [
-		{
-			key: String,
-			name: String,
-			shortcut: String,
-			value: Number,
-			commentary: String,
-		}
-	],
+	name: {
+		type: String,
+		required: 'Please enter a name of the instrument'
+	},
+	onFire: {
+		type: Boolean,
+		default: false
+	},
+	timeFrames: {
+		type: Array,
+		default: [
+			{
+				key: 'hour',
+				name: '1 Hour',
+				shortcut: '1H',
+				value: 2,
+				comment: "lorem"
+			},
+			{
+				key: 'fourHour',
+				name: '4 Hour',
+				shortcut: '4H',
+				value: 2,
+				comment: "lorem"
+			},
+			{
+				key: 'week',
+				name: 'Week',
+				shortcut: '1W',
+				value: 2,
+				comment: "lorem"
+			},
+			{
+				key: 'month',
+				name: 'Month',
+				shortcut: '1M',
+				value: 2,
+				comment: "lorem"
+			}
+		]
+	},
 	fundamentals: {
 		comment: String
 	}
--- server.js
+++ server.js
@@ -1,36 +1,77 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const mongoose = require('mongoose');
 const connectionString = 'mongodb+srv://admin:Adik3714@cluster0-p1zzs.mongodb.net/FinancialInstrumentsData?retryWrites=true&w=majority';
 const Instrument = require('./backend/models/instrument');
-
+const BodyParser = require('body-parser');
+const app = express();
+// Initialization
 app.use(express.static("./frontend/dist"));
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({extended: true}));
+mongoose.connect(connectionString, {useNewUrlParser: true});
 
-app.get('/', function (req, res) {
-
-	/*mongoose.connect(connectionString, {useNewUrlParser: true})
-		.catch(err => console.log(err))
-		.then(con => console.log(con));
-
-	const eurusd = new Instrument({
-		name: "EUR/USD",
-		onFire: true,
-		timeFrames: [
-			{
-				key: 'hour',
-				name: '',
-				shortcut: '1H',
-				value: 1,
-				commentary: 'Lorem ipsum'
-			}
-		]
-	});
-
-	console.log(eurusd);
-
-	eurusd.save().then((es) => console.log("success")).catch((err) => console.log(err));*/
-
+/**
+ * Get all available instruments
+ */
+app.get('/api/instruments', async (request, response) => {
+	try {
+		const instruments = await Instrument.find().exec();
+		response.send(instruments);
+	} catch (err) {
+		response.status(500).send(err);
+	}
 });
+
+/**
+ * Create new instrument
+ */
+app.post('/api/instrument', async (request, response) => {
+	try {
+		const instrument = new Instrument(request.body);
+		const result = await instrument.save();
+		response.send(result);
+	} catch (err) {
+		response.status(500).send(err);
+	}
+});
+
+/**
+ * Update desired instrument
+ */
+app.put('/api/instrument/:id', async (request, response) => {
+	try {
+		const instrument = await Instrument.findById(request.params.id).exec();
+		instrument.set(request.body);
+		const result = await instrument.save();
+		response.send(result)
+	} catch (err) {
+		response.status(500).send(err);
+	}
+});
+
+/**
+ * Deleting one instrument record by id
+ */
+app.delete('/api/instrument/:id', async (request, response) => {
+	try {
+		const result = await Instrument.deleteOne({_id: request.params.id}).exec();
+		response.send(result);
+	} catch (err) {
+		response.status(500).send(err);
+	}
+});
+
+/**
+ * Deleting all
+ */
+app.delete('/api/instruments', async (request, response) => {
+	try {
+		const result = await Instrument.deleteMany({});
+		response.send(result);
+	} catch (err) {
+		response.status(500).send(err);
+	}
+})
 
 app.listen(3000);
